efc3eb396f5a637634c6a2ce9272dec7b70e50ac
Tom <tomhemery@outlook.com>
2019-11-06 01:30:20 +0000

    Started adding radar, including skeleton script and sprite asset

---
+++ Assets/Scripts/UI/Radar.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radar : MonoBehaviour
+{
+    public static Radar Instance { get; private set; } = null;
+
+    private List<Transform> targets = new List<Transform>();
+    private List<GameObject> radarPings = new List<GameObject>();
+
+    public Transform PlayerShip;
+    public float RadarRange = 100;
+    public GameObject RadarPingObject;
+
+    private void Awake()
+    {
+        if (Instance != null) Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform t;
+        for (int i = 0; i < targets.Count; i++)
+        {
+            t = targets[i];
+            GameObject ping = radarPings[i];
+            if (Vector2.Distance(t.position, PlayerShip.position) < RadarRange)
+            {
+
+                ping.SetActive(true);
+                
+            }
+            else ping.SetActive(false);
+        }
+    }
+
+    public void AddTarget(Transform t)
+    {
+        targets.Add(t);
+        GameObject ping = Instantiate(RadarPingObject, transform);
+        radarPings.Add(ping);
+        ping.SetActive(false);
+    }
+}
