d3f7e74cb4f5552aa807327125eae1711dad5f71
Tom <tomhemery@outlook.com>
2020-02-17 23:50:28 +0000

    Added inventory script, drops to asteroid, inventory ui sprites

--- Assets/Scripts/InventoryAndCollectables/Inventory.cs
+++ Assets/Scripts/InventoryAndCollectables/Inventory.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    [SerializeField]
-    public Dictionary<GameObject, int> Contents { get; private set; } = new Dictionary<GameObject, int>();
+    public Dictionary<string, int> Contents { get; private set; } = new Dictionary<string, int>();
     public int FilledCapacity { get; private set; } = 0;
     public int MaxCapacity = 100;
 
@@ -15,10 +14,13 @@ public class Inventory : MonoBehaviour
     /// <param name="item">The item we're adding</param>
     /// <param name="quantity">The amount we're adding (default 1)</param>
     /// <returns>True if there is space, false otehrwise</returns>
-    public bool TryAddItem(GameObject item, int quantity = 1) {
+    public bool TryAddItem(string item, int quantity = 1) {
         if (MaxCapacity > FilledCapacity + quantity)
         {
-            Contents[item] += quantity;
+            if (Contents.ContainsKey(item))
+                Contents[item] += quantity;
+            else
+                Contents.Add(item, quantity);
             FilledCapacity += quantity;
             return true;
         }
@@ -28,10 +30,10 @@ public class Inventory : MonoBehaviour
     /// <summary>
     /// Checks if the inventory contains "quantity" of "item", if it does then it removes that amount of item from the inventory
     /// </summary>
-    /// <param name="item">The GameObject we're looking for</param>
+    /// <param name="item">The item we're looking for</param>
     /// <param name="quantity">The amount of the item we want</param>
     /// <returns>True if there is enough of the item in the inventory, false otherwise</returns>
-    public bool TryRemoveItem(GameObject item, int quantity = 1)
+    public bool TryRemoveItem(string item, int quantity = 1)
     {
         bool result = CheckForItem(item, quantity);
         if (result) {
@@ -42,7 +44,13 @@ public class Inventory : MonoBehaviour
         return result;
     }
 
-    public bool CheckForItem(GameObject item, int quantity = 1) {
+    /// <summary>
+    /// Checks if "quantiy" of "item" are present in the inventory
+    /// </summary>
+    /// <param name="item"></param>
+    /// <param name="quantity"></param>
+    /// <returns></returns>
+    public bool CheckForItem(string item, int quantity = 1) {
         return (Contents.ContainsKey(item) && Contents[item] >= quantity);
     }
 }
