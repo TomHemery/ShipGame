91bc0f6c06e4be05904991a50d793ee75ea72abd
Tom <tomhemery@outlook.com>
2020-05-08 15:54:27 +0100

    Replaced the font with a more spacey one

--- Assets/Scripts/Saves/Save.cs
+++ Assets/Scripts/Saves/Save.cs
@@ -97,7 +97,6 @@ public class Save
         file.Close();
 
         Debug.Log("Saved Game!");
-        Debug.Log("Save jump drive fill level: " + save.jumpDriveFillLevel + " save jump refueller contents: " + save.jumpDriveContents);
     }
 
     /// <summary>
---
+++ Assets/Scripts/UI/InventoryAndEquipment/InventoryTextTracker.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryTextTracker : MonoBehaviour
+{
+
+    public Inventory associatedInventory;
+    public bool attachToPlayer;
+    public Text textTarget;
+    public string messagePrefix;
+
+    public Color defaultColour;
+    public Color fullColour;
+
+    private void Awake()
+    {
+        if (attachToPlayer) associatedInventory = GameObject.FindGameObjectWithTag("PlayerShip").GetComponent<Inventory>();
+        associatedInventory.InventoryChangedEvent += OnAssociatedInventoryChanged;
+
+        if (textTarget == null) {
+            textTarget = GetComponent<Text>();
+            if (textTarget == null) Debug.LogError("No text target set for text tracker", this);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        associatedInventory.InventoryChangedEvent -= OnAssociatedInventoryChanged;
+        associatedInventory = null;
+    }
+
+    private void OnEnable()
+    {
+        Refresh();
+    }
+
+    public void OnAssociatedInventoryChanged(object sender, EventArgs e)
+    {
+        Refresh();
+    }
+
+    public void Refresh()
+    {
+        textTarget.text = messagePrefix + associatedInventory.FilledCapacity.ToString() + "/" + associatedInventory.MaxCapacity.ToString();
+        textTarget.color = associatedInventory.FilledCapacity >= associatedInventory.MaxCapacity ? fullColour : defaultColour;
+    }
+}
